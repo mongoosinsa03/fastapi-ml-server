f38c303c841c2c7b402f88092b193f5c25b7c6e4
mongoosinsa03 <113415622+mongoosinsa03@users.noreply.github.com>
2025-11-23 07:35:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from fastapi import FastAPI
-import joblib
 
 app = FastAPI()
 
-# Load models
-scaler = joblib.load("scaler.joblib")
-kmeans = joblib.load("kmeans.joblib")
-
 @app.get("/")
-def home():
-    return {"message": "FastAPI ML server alive!"}
+def root():
+    return {"message": "Hello world"}
 
 @app.post("/predict")
-def predict(features: dict):
-    import numpy as np
-
-    X = np.array([list(features.values())]).reshape(1, -1)
-    X_scaled = scaler.transform(X)
-    cluster = kmeans.predict(X_scaled)[0]
-
-    return {"cluster": int(cluster)}
+def predict(data: InputData):
+    ...
